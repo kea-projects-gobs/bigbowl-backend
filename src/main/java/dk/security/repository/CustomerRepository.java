256a2e7bc82ca16208dfd5790faff898e451b29a
osman-butt <butt.osman@gmail.com>
2024-05-14 13:33:59 +0200

    removed redundant files

--- src/main/java/dk/security/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package dk.security.repository;
-
-import dk.security.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,String> {
-}
