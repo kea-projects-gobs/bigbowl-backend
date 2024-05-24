d0ee62ead6ff8b9451479141d14d30cb7a47c886
osman-butt <butt.osman@gmail.com>
2024-05-24 13:07:29 +0200

    Update reservation service

--- src/main/java/dk/bigbowl/reservation/repository/ReservationItemRepository.java
+++ src/main/java/dk/bigbowl/reservation/repository/ReservationItemRepository.java
@@ -2,10 +2,19 @@ package dk.bigbowl.reservation.repository;
 
 import dk.bigbowl.reservation.entity.ReservationItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReservationItemRepository extends JpaRepository<ReservationItem, Long> {
     List<ReservationItem> findByActivityIdAndStartTimeLessThanAndEndTimeGreaterThan(Long activityId, LocalDateTime startTime, LocalDateTime endTime);
+    List<ReservationItem> findByActivityIdAndStartTime(Long activityId, LocalDateTime date);
+    @Query("SELECT r FROM ReservationItem r WHERE " +
+            "(:startTime < r.endTime AND :endTime > r.startTime) and r.activity.type.name = :activityType")
+    List<ReservationItem> findOverlappingReservations(
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime,
+            @Param("activityType") String activityType);
 }
--- src/main/java/dk/bigbowl/reservation/service/ReservationService.java
+++ src/main/java/dk/bigbowl/reservation/service/ReservationService.java
@@ -1,5 +1,6 @@
 package dk.bigbowl.reservation.service;
 
+import dk.bigbowl.reservation.dto.ReservationQuoteResDto;
 import dk.bigbowl.reservation.dto.ReservationRequest;
 import dk.bigbowl.reservation.dto.ReservationResponse;
 import dk.bigbowl.reservation.entity.Reservation;
@@ -7,7 +8,5 @@ import dk.bigbowl.reservation.entity.Reservation;
 import java.security.Principal;
 
 public interface ReservationService {
-    ReservationResponse createReservation(ReservationRequest reservationRequest, Principal principal);
-    ReservationResponse convertToDTO(Reservation reservation);
-    Reservation convertToEntity(ReservationRequest reservationRequest, Principal principal);
+    ReservationResponse createReservation(ReservationQuoteResDto reservationRequest, Principal principal);
 }
