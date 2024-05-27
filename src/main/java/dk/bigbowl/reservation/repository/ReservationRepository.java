27fdfa6f8f37298b482eca1e9a0df4e5cf446d3a
osman-butt <butt.osman@gmail.com>
2024-05-28 01:19:51 +0200

    Added Search reservations

--- src/main/java/dk/bigbowl/reservation/repository/ReservationRepository.java
+++ src/main/java/dk/bigbowl/reservation/repository/ReservationRepository.java
@@ -3,9 +3,13 @@ package dk.bigbowl.reservation.repository;
 import dk.bigbowl.reservation.entity.Reservation;
 import dk.security.entity.UserWithRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByUser(UserWithRoles user);
+    @Query("SELECT r FROM Reservation r WHERE r.date >= :from AND r.date <= :to")
+    List<Reservation> findByDateRange(LocalDate from, LocalDate to);
 }
