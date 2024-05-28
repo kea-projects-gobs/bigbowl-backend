debc9f901312974ce52fd2c72494e8e9a0c3437f
osman-butt <butt.osman@gmail.com>
2024-05-28 11:34:45 +0200

    Updated CORS settings

--- src/main/java/dk/security/config/CorsConfig.java
+++ src/main/java/dk/security/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     //When you know the domain name for your client, add it below before deployment
     private static final String[] ALLOWED_ORIGINS = new String[] {
             //"*",
-            "https://example.com/",
+            "https://bigbowl.onrender.com/",
             "http://localhost:5173",
             "http://localhost:5174",
     };
